4cdb094661a7d6a7390fb29c52903bfab242ff12
Luke <lukemckinstry@gmail.com>
2018-03-18 10:10:45 -0400

    everything before react-faux-dom changes

--- my-app/src/Chart.js
+++ my-app/src/Chart.js
@@ -19,9 +19,9 @@ class Chart extends React.Component{
     	}
    }
 
-    draw( data ) {
+    draw( ) {
     	console.log( "DRAW" );
-    	console.log(data)
+    	
     	var margin = {top: 10, right: 80, bottom: 45, left: 80},
 	    width = 1200 - margin.left - margin.right,
 	    height = 600 - margin.top - margin.bottom;
@@ -42,7 +42,7 @@ class Chart extends React.Component{
 		        .ticks(10);
 		}
 
-		console.log( myData )
+		//console.log( myData )
 
 		myData.forEach(function(d) {
 		  d.year = +d.year;
@@ -54,7 +54,7 @@ class Chart extends React.Component{
 		    });
 
 		// Scale the range of the data
-	    x.domain(d3.extent(data, function(d) { return d.year; }));
+	    x.domain(d3.extent(myData, function(d) { return d.year; }));
 	    y.domain([0, 1]);
 
 	    // Nest the entries by city
@@ -63,28 +63,19 @@ class Chart extends React.Component{
 	            return d.city;})
 	        .entries(myData);
 
-	    console.log( dataNest )
+	    //console.log( dataNest )
 
-
-
-		
-
-		// var xAxis = d3.axisBottom()
-		// 	.scale(x).orient("bottom").ticks(10);      
-		// var yAxis = d3.axisLeft()
-		// 	.scale(y).orient("left").ticks(10);
+		var xAxis = d3.axisBottom()
+			.scale(x).ticks(10);      
+		var yAxis = d3.axisLeft()
+			.scale(y).ticks(10);
 
 		// Define the line
-		// var PCTline = d3.line()
-		//     .x(function(d) { return x(d.year); })
-		//     .y(function(d) { return y(d.pct); });
+		var PCTline = d3.line()
+		    .x(function(d) { return x(d.year); })
+		    .y(function(d) { return y(d.pct); });
 
 		const noode = this.noode
-
-		//define div for the tooltip
-		// var div = d3.select(noode).append("div")
-		//     .attr("class", "tooltip")
-		//     .style("opacity", 0);
 
 		// Adds the svg canvas
 		var svg = d3.select(noode)
@@ -94,6 +85,41 @@ class Chart extends React.Component{
 		    .append("g")
 		        .attr("transform",
 		              "translate(" + margin.left + "," + margin.top + ")");
+
+		//define div for the tooltip
+		var div = d3.select("svg-container").append("div")
+		    .attr("class", "tooltip")
+		    .style("opacity", 0);
+
+		dataNest.forEach(function(d, i) {
+	        svg.append("path")
+	            .attr("class", "line")
+	            .style("stroke", "black")
+	            .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
+	            .attr("d", PCTline(d.values))
+	            .style('opacity', 0);
+
+	        var legendSpace = height/dataNest.length; // spacing for legend
+
+	        // Add the Legend
+	        svg.append("text")
+	            .attr("x", 0 - margin.left/2)
+	            .attr("y", (legendSpace/2)+i*legendSpace)
+	            .attr("class", "legend")
+	            .style("fill", "black")
+	            .on("click", function(){
+	                // Determine if current line is visible
+	                var active   = d.active ? false : true,
+	                newOpacity = active ? 0 : 1;
+	                // Hide or show the elements based on the ID
+	                d3.select("#tag"+d.key.replace(/\s+/g, ''))
+	                    .transition().duration(100)
+	                    .style("opacity", newOpacity);
+	                // Update whether or not the elements are active
+	                d.active = active;
+	                })
+	            .text(d.key);
+	        });
 
 		//Add the grid
 	    svg.append("g")
@@ -144,6 +170,57 @@ class Chart extends React.Component{
               }
 
             })
+            .on("mouseover", function(d) {
+	            console.log( d );
+	            div.transition()
+	                .ease("elastic")
+	                .duration(200)
+	                .style("opacity", .9);
+	            div .html((d.city) + "<br/>"  + d.year + "<br/>" + "Championships: " + d.champ
+	                + "<br/>" + "Top 4 Finishes: " + d.ff + "<br/>" + "Teams: " + d.tooltip )
+	                .style("left", (d3.event.pageX) + "px")
+	                .style("top", (d3.event.pageY - 28) + "px");
+	            })
+            .on("mouseout", function(d) {
+                console.log( d );
+                div.transition()
+                    .ease("elastic")
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+        // Add the X Axis
+	    svg.append("g")
+	        .attr("class", "x axis")
+	        .attr("transform", "translate(0," + height + ")")
+	        .call(xAxis)
+	        .selectAll("text")
+	            .style("text-anchor", "end")
+	            .attr("dx", "-.8em")
+	            .attr("dy", ".15em")
+	            .attr("transform", function(d) {
+	                return "rotate(-65)"
+	                });
+
+	    // Add the Y Axis
+	    svg.append("g")
+	        .attr("class", "y axis")
+	        .attr("transform", "translate(" + width + ",0)")
+	        .call(yAxis);
+
+	        var padding = 100
+	        svg.append("text")
+	            .attr("text-anchor", "middle")
+	            // this makes it easy to centre the text as the transform is applied to the anchor
+	            .attr("transform", "translate("+ (width + margin.right - 7) +","+(60)+")rotate(-90)")// text is drawn off the screen top left, move down and out and rotate
+	            .text("City Winning Percentage");
+
+	        svg.append("text")
+	            .attr("text-anchor", "middle")
+	            // this makes it easy to centre the text as the transform is applied to the anchor
+	            .attr("transform", "translate("+ (width - 23) +","+(height + margin.bottom)+")")
+	            // centre below axis
+	            .text("Season");
 
 
 	}
@@ -154,6 +231,7 @@ class Chart extends React.Component{
             	<svg ref={node => this.noode = node}
 	        	width={500} height={500}>
 	        	</svg>
+	        	
         </div>
       )}
 }
